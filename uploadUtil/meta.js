6d1b2614dc27f276d67899eb7198e8ac982b865c
Leo Kim <me@leok.kr>
2021-09-13 03:15:39 +0900

    feat: add more meta, version/link

--- uploadUtil/meta.js
+++ uploadUtil/meta.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 export async function getMeta(appName) {
-  var meta = { artwork: undefined, bundleID: undefined, genres: undefined };
+  var meta = {
+    artwork: undefined,
+    bundleID: undefined,
+    genres: undefined,
+    trackName: undefined,
+    latestVersion: undefined,
+    artistName: undefined,
+    link: undefined,
+  };
   const response = await axios.get(
     encodeURI(
       `http://itunes.apple.com/search?term=${appName}&country=US&media=software&limit=1`
@@ -14,6 +22,10 @@ export async function getMeta(appName) {
     meta.artwork = metaData[0].artworkUrl100;
     meta.bundleID = metaData[0].bundleId;
     meta.genres = metaData[0].genres;
+    meta.trackName = metaData[0].trackName;
+    meta.latestVersion = metaData[0].version;
+    meta.artistName = metaData[0].artistName;
+    meta.link = metaData[0].trackViewUrl;
   }
   return meta;
 }
